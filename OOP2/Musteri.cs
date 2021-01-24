731a3c8b909fa266b4336a5feef6013400ec3e02
fatihelbl <68300579+fatihelbl@users.noreply.github.com>
2021-01-24 17:13:21 +0300

    23 Ocak ders sonu

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+
+        // eğerki bir nesnede bir değeri kullanmak zorunda değilsen
+        // demek ki orda hata yapıyorsun!!
+
+
+
+
+    }
+}
